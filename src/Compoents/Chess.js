63eb965a77e17116fe01dd804a1ff497f8c09960
rushikesh Pawar <rushikesh.pawarms@gmail.com>
2022-05-06 09:47:45 +0530

    testing perpose

--- src/Compoents/Chess.js
+++ src/Compoents/Chess.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Chess.css'
+export default function Chess() {
 
-function Chess() {
+  const inputRef = React.useRef(null);
+  React.useEffect(() => {
+    console.log(inputRef);
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <div><h1> Loading.....</h1></div>
+    <div className='zero' ref={inputRef}>
+      <form className='first'>
+      <h1>Hello this is for only testing perpose</h1>
+      <input className='second'
+        type="text"
+        placeholder="Enter Name" 
+        name="name" 
+        />
+    </form>
+    </div>
   )
 }
-
-export default Chess
